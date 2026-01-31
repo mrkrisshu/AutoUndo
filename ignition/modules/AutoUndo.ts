11ba3f9fabbc4e7c9861551b3992e2f6b0407a55
Krishna Bantola D <67815735+mrkrisshu@users.noreply.github.com>
2026-01-31 14:49:16 +0530

    fix: Remove hardhat config files for Vercel build

--- ignition/modules/AutoUndo.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const AutoUndoModule = buildModule("AutoUndoModule", (m) => {
-    const autoUndoStorage = m.contract("AutoUndoStorage");
-    return { autoUndoStorage };
-});
-
-export default AutoUndoModule;
